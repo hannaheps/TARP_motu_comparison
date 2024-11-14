6d4c0ab7398db7b56b71bb2fef0f6aea92a6c6eb
Hannah Epstein <hannah@Hannahs-MacBook-Pro.local>
2024-11-14 00:04:40 +0000

    update to wind code

--- wind_data/procedure/plot_tetiaroa_wind.R
+++ wind_data/procedure/plot_tetiaroa_wind.R
@@ -1,10 +1,14 @@
 ## Code for wind data
+library(ggplot2)
+library(plyr)
 
+#Read in data from Meteo France (open access online: https://meteo.data.gouv.fr/form)
 tet_weather <- read.csv("../input/Tet_Weather_2020_to_2022.csv")
 
-
+#Subset to november 2021 only
 tet_weather_nov <- tet_weather %>% filter(AAAAMM == "2021_11")
 
+#Explore data
 sum.FF <- ddply(tet_weather_nov, "JJ", summarise,
                  N = length(FF),
                  mean = mean(FF),
@@ -12,6 +16,7 @@ sum.FF <- ddply(tet_weather_nov, "JJ", summarise,
                  se = sd/sqrt(N)
 )
 
+pdf(file = "../output/windspeed_tet_november2021.pdf")
 ggplot(sum.FF, aes(x = JJ, y = mean)) +
   geom_bar(stat = "identity") +
   ggtitle("FF in November at Tetiaroa") +
@@ -20,7 +25,7 @@ ggplot(sum.FF, aes(x = JJ, y = mean)) +
   ylab("FF") +
   theme_classic() +
   theme(panel.border = element_rect(color = "black", fill = NA, linetype = 1, linewidth = 0.5))
-
+dev.off()
 
 
 sum.DD <- ddply(tet_weather_nov, "JJ", summarise,
@@ -30,6 +35,7 @@ sum.DD <- ddply(tet_weather_nov, "JJ", summarise,
                 se = sd/sqrt(N)
 )
 
+pdf(file = "../output/winddirection_tet_november2021.pdf")
 ggplot(sum.DD, aes(x = JJ, y = mean)) +
   geom_bar(stat = "identity") +
   ggtitle("DD in November at Tetiaroa") +
@@ -38,9 +44,32 @@ ggplot(sum.DD, aes(x = JJ, y = mean)) +
   ylab("DD") +
   theme_classic() +
   theme(panel.border = element_rect(color = "black", fill = NA, linetype = 1, linewidth = 0.5))
-library(ggplot2)
+dev.off()
 
-plot.windrose()
+##Use the windrose code
+source(windrose.R)
 
 plot.windrose(spd = tet_weather_nov$FF,
                     dir = tet_weather_nov$DD)
+
+#Remove NAs in the speed (FF) and direction (DD) columns
+tet_weather_nov_narm <- tet_weather_nov[!is.na(tet_weather_nov$FF) | !is.na(tet_weather_nov_narm$DD), ]
+
+#build the windrose, using customised binning for speed
+pdf(file = "../output/windrose_tet_november2021_custbin.pdf")
+plot.windrose(data = tet_weather_nov_narm, 
+              spd = "FF",
+              dir = "DD",
+              spdseq = c(0.0,3,6,9,12))
+dev.off()
+
+
+#Can make it ggplot compatible: 
+p.wind <- plot.windrose(data = tet_weather_nov_narm, 
+                        spd = "FF",
+                        dir = "DD",
+                        spdseq = c(0.0,3,6,9,12))
+
+p.wind + theme(axis.title.x = element_blank(), axis.text.x = element_blank())
+
+
